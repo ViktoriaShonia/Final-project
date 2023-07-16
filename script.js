b3a4c64a09abbeffdedd4a03f73f4cac633cf591
ViktoriaShonia <135555714+ViktoriaShonia@users.noreply.github.com>
2023-07-16 21:47:42 +0400

    script.js

--- script.js
+++ script.js
@@ -32,3 +32,23 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
 });
+
+function enlargeImage(image) {
+    const enlargedContainer = document.getElementById('enlarged-container');
+    enlargedContainer.innerHTML = `<img src="${image.src}" class="enlarged-image">`;
+    enlargedContainer.style.display = 'flex';
+  }
+  
+  function cancelEnlarge() {
+    const enlargedContainer = document.getElementById('enlarged-container');
+    enlargedContainer.style.display = 'none';
+    enlargedContainer.innerHTML = '';
+  }
+  
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+   
+    var navbar = document.querySelector('.navbar');
+    navbar.style.display = navbar.style.display === 'none' ? 'flex' : 'none';
+  });
+  
